812f5bd7f82b85ba762441eaf0062f3a3a138ed5
Mohammad Sajjad Nejati <nejati.mohammadsajjad@gmail.com>
2020-06-05 22:23:56 +0430

    Resolve bugs

--- user.h
+++ user.h
@@ -24,7 +24,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-int pinfo(struct proc_info*);
+int procInfo(struct proc_info*);
 
 // ulib.c
 int stat(const char*, struct stat*);

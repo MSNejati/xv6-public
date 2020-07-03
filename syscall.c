bc4d6d8bec92b9add38daab3df9c05f8872c3c19
Mohammad Sajjad Nejati <nejati.mohammadsajjad@gmail.com>
2020-07-03 23:11:16 +0430

    Edit syscall.c and add function prototype here

--- syscall.c
+++ syscall.c
@@ -104,6 +104,7 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_procInfo(void);    // Add prototype for new system call named "procInfo"
+extern int sys_waitx(void);       // Add prototype for new system call named "waitx"
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -128,6 +129,7 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_procInfo] sys_procInfo,     // Add pointer for new system call function here
+[SYS_waitx] sys_waitx,           // Add pointer for new system call function here
 };
 
 void
